c7f1094f91e3e5760a08cc15cb9bd5c24916d41b
Julio <jtorres1989@gmail.com>
2018-11-27 10:14:33 -0500

    created express js server and production build

---
+++ build/precache-manifest.ab5c76c52711bb59df63dca0c5742637.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4d9db59d9a1f6666e79",
+    "url": "/static/js/main.f4d9db59.chunk.js"
+  },
+  {
+    "revision": "4d8df4558ceb3295c536",
+    "url": "/static/js/1.4d8df455.chunk.js"
+  },
+  {
+    "revision": "f4d9db59d9a1f6666e79",
+    "url": "/static/css/main.f82a04ff.chunk.css"
+  },
+  {
+    "revision": "c0dd4ee0a925eb7dfe1b0611b73d98cb",
+    "url": "/index.html"
+  }
+];
